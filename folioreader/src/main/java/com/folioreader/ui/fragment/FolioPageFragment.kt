5d4a6c00e7b672ab13a073050ad6944825989ec3
TUANNKA\anhtu <tuan.nguyen991@hcmut.edu.vn>
2023-04-17 16:40:39 +0700

    Fix bug ReadPage incorrect

--- folioreader/src/main/java/com/folioreader/ui/fragment/FolioPageFragment.kt
+++ folioreader/src/main/java/com/folioreader/ui/fragment/FolioPageFragment.kt
@@ -682,6 +682,10 @@ class FolioPageFragment(private var pageViewModel: PageTrackerViewModel) : Fragm
             val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)!!
             var readPage = sharedPref.getInt("readPage", 0)
 
+            val editor: SharedPreferences.Editor = sharedPref.edit()
+            editor.putInt("readPage", 0)
+            editor.commit()
+
             var href = spineItem.href
             if (href == null) href = ""
             val created = Date().time
